57f55b254269f266ca91884136d126541a8cb39c
Shane Claussen <sclaussen@yahoo.com>
2023-12-26 17:13:10 -0800

    Minor updates

--- nutrients.js
+++ nutrients.js
@@ -17,30 +17,34 @@ main()
 
 
 async function main() {
-    let ingredients = YAML.load(fs.readFileSync('./ingredients.yaml', 'utf8'));
-    let nutrients = YAML.load(fs.readFileSync('./nutrients.yaml', 'utf8'));
+    let config = YAML.load(fs.readFileSync('./nutrients.yaml', 'utf8'));
+    let ingredients = config.ingredients;
+    let nutrients = config.nutrients;
 
     for (let ingredient of ingredients) {
         if (ingredient.skip) {
             continue
         }
 
+        // Retrieve the URL from whole foods
         let body = (await curl.get(ingredient.url)).body
-        body = body.replace(/<[^<]+>/g, "")
-        body = body.replace(/\n/g, "")
-        body = body.replace(/\n/g, "")
-        body = body.replace(/\\/g, "")
+
+
+        // Remove a bunch of noise in the response
+        body = body.replace(/<[^<]+>/g, '')
+        body = body.replace(/\n/g, '')
+        body = body.replace(/\n/g, '')
+        body = body.replace(/\\/g, '')
         body = body.replace(/.+{"props":/g, "{\"props\":")
         body = body.replace(/"globalAlert.*$/, "")
         body = body.replace(/,$/, "}}}")
-        // p4(body)
 
-        let wholeFoods = JSON.parse(body)
-        p4(wholeFoods)
-        let wf = wholeFoods.props.pageProps.data
+        let wf = JSON.parse(body).props.pageProps.data
+        p4(wf)
 
 
         let data = []
+        let warnings = []
 
 
         // name (from our input YAML file)
@@ -52,6 +56,7 @@ async function main() {
         data.push({ name: 'name', value: ingredient.name })
         data.push({ name: 'fullName', value: mapName(wf.name) })
         data.push({ name: 'url', value: ingredient.url })
+        data.push({ name: 'note', value: ingredient.note || '' })
         data.push({ name: 'imageUrl', value: wf.images[0].image })
         data.push({ name: 'brand', value: mapBrand(wf.brand.name) })
         data.push({ name: 'category', value: mapCategory(wf.categories.name) })
@@ -61,15 +66,19 @@ async function main() {
         // Ingredients
         let ingredients = []
         for (let ingredient of wf.ingredients) {
-            ingredient = ingredient.replace(/(?<=[^(]*\([^()]*),(?=[^)]*\))/g, ";")
-            ingredient = ingredient.replace(/(?<=[^(]*\([^()]*)\.(?=[^)]*\))/g, "")
-            ingredient = ingredient.replace(/(?<=[^\[]*\[[^\[\]]*),(?=[^\]]*\])/g, ";")
-            ingredient = ingredient.replace(/(?<=[^\[]*\[[^\[\]]*)\.(?=[^\]]*\])/g, "")
+            ingredient = ingredient.replace(/(?<=[^(]*\([^()]*),(?=[^)]*\))/g, ';')
+            ingredient = ingredient.replace(/(?<=[^(]*\([^()]*)\.(?=[^)]*\))/g, '')
+            ingredient = ingredient.replace(/(?<=[^\[]*\[[^\[\]]*),(?=[^\]]*\])/g, ';')
+            ingredient = ingredient.replace(/(?<=[^\[]*\[[^\[\]]*)\.(?=[^\]]*\])/g, '')
             for (let separatedIngredient of ingredient.split(/,/)) {
                 separatedIngredient = separatedIngredient.trim()
-                separatedIngredient = separatedIngredient.replace(/Ingredients: /, "")
-                separatedIngredient = separatedIngredient.replace(/\.$/, "")
+                separatedIngredient = separatedIngredient.replace(/Ingredients: /, '')
+                separatedIngredient = separatedIngredient.replace(/\.$/, '')
                 ingredients.push(separatedIngredient)
+
+                if (separatedIngredient.includes('cane')) {
+                    warnings.push(separatedIngredient);
+                }
             }
         }
         data.push({ name: 'ingredients', value: ingredients })
@@ -85,40 +94,56 @@ async function main() {
         data.push({ name: 'allergens', value: allergens })
 
 
-        // Consumption Unit
-        if (ingredient.consumptionUnit) {
-            data.push({ name: 'consumptionUnit', value: ingredient.consumptionUnit })
-            data.push({ name: 'consumptionGrams', value: ingredient.consumptionGrams })
-        } else {
-            data.push({ name: 'consumptionUnit', value: "grams" })
-            data.push({ name: 'consumptionGrams', value: "1" })
-        }
-        p4(data);
-
-
-        let macroNutrientData = []
-
-
         // Serving Size
-        let servingSize = wf.servingInfo.secondaryServingSize
-        if (wf.servingInfo.secondaryServingSizeUom !== 'g' && wf.servingInfo.secondaryServingSizeUom !== 'G') {
-            if (!ingredient['conversion_factor']) {
-                console.error('ERROR: No conversion factor exists for ' + ingredient.name)
-                console.error(wf.servingInfo);
-                // process.exit(1)
-            } else {
-                servingSize *= ingredient.conversion_factor
+        // Normal:
+        // {
+        //   servingSize: 1,
+        //   servingSizeUom: 'cup',
+        //   secondaryServingSize: 144,
+        //   secondaryServingSizeUom: 'g',
+        //   totalSize: 12,
+        //   totalSizeUom: 'oz',
+        //   secondaryTotalSize: 340,
+        //   secondaryTotalSizeUom: 'g',
+        //   servingsPerContainerDisplay: 'Varies'
+        // }
+        //
+        // Oils:
+        // {
+        //   servingSize: 1,
+        //   servingSizeUom: 'tbsp',
+        //   servingSizeDisplay: '1',
+        //   secondaryServingSize: 15,
+        //   secondaryServingSizeUom: 'ml',
+        //   secondaryServingSizeDisplay: '15',
+        //   totalSize: 16.9,
+        //   totalSizeUom: 'fl oz',
+        //   secondaryTotalSize: 500,
+        //   secondaryTotalSizeUom: 'ml',
+        //   servingsPerContainer: 33,
+        //   servingsPerContainerDisplay: '33'
+        // }
+        if (_.has(ingredient, 'consumptionUnit')) {
+            data.push({ name: 'consumptionUnit', value: ingredient.consumptionUnit });
+            data.push({ name: 'consumptionGrams', value: ingredient.consumptionGrams });
+            data.push({ name: 'servingSize', value: ingredient.servingSize })
+        } else {
+            if (wf.servingInfo.secondaryServingSizeUom.toLowerCase() !== 'g') {
+                console.error('ERROR: Unknown serving size unit of measure for ' + nutrient.name + ': ' + wf.servingInfo);
+                process.exit(1)
             }
+            data.push({ name: 'consumptionUnit', value: 'gram' });
+            data.push({ name: 'consumptionGrams', value: 1 });
+            data.push({ name: 'servingSize', value: wf.servingInfo.secondaryServingSize })
         }
-        data.push({ name: "servingSize", value: servingSize })
 
 
-        p4(_.map(wf.nutritionElements, item => _.pick(item, [ 'key', 'uom', 'perServing', 'perServingDisplay' ])));
+        // p4(_.map(wf.nutritionElements, item => _.pick(item, [ 'key', 'uom', 'perServing', 'perServingDisplay' ])));
         for (let nutrient of nutrients) {
 
             let wfNutrient = _.find(wf.nutritionElements, { key: nutrient.wf_key })
             if (!wfNutrient) {
-                console.log('Could not find: ' + nutrient.wf_key);
+                // console.log('Could not find: ' + nutrient.wf_key);
                 if (nutrient.required) {
                     process.exit(1);
                 }
@@ -136,7 +161,7 @@ async function main() {
             // if required.
             if (wfNutrient.uom !== nutrient.unit) {
 
-                if (_.has(nutrient, 'alternative_unit')) {
+                if (!_.has(nutrient, 'alternate_unit')) {
                     console.error('ERROR: The Whole Foods nutrient\'s unit of measure is unknown: ' + nutrient.name + '/' + wfNutrient.uom)
                     process.exit(1)
                 } else if (wfNutrient.uom !== nutrient.alternate_unit.unit) {
@@ -156,15 +181,17 @@ async function main() {
 
 
         // Add netcarbs to the macro nutrients
-        p4(data)
         let carbs = _.find(data, { name: 'carbohydrates' });
-        let fiber = _.find(data, { name: 'fiber' });
+        let fiber = _.find(data, { name: 'fiber' }) || { value: 0 };
         if (carbs) {
-            data.push({ name: "netCarbs", value: carbs.value - fiber.value })
+            data.push({ name: 'netCarbs', value: carbs.value - fiber.value })
         }
 
 
-        p4(_.map(wf.nutritionElements, item => _.pick(item, [ 'key', 'uom', 'perServing', 'perServingDisplay' ])));
+        // Warnings
+        data.push({ name: 'warnings', value: warnings });
+
+        // p4(_.map(wf.nutritionElements, item => _.pick(item, [ 'key', 'uom', 'perServing', 'perServingDisplay' ])));
         serialize(data)
         console.log()
     }
@@ -197,20 +224,17 @@ function mapBrand(brand) {
 
 
 function serialize(data) {
-    p(data)
+    // p(data)
 
-    y(data, 'name', '', '- ')
-    y(data, 'brand')
-    y(data, 'fullName')
-    y(data, 'url')
-    y(data, 'totalCost')
-    y(data, 'totalGrams')
-    y(data, 'category')
-    y(data, 'ingredients')
-    y(data, 'allergens')
-
-    y(data, 'consumptionUnit')
-    y(data, 'consumptionGrams')
+    y(data, 'name')
+    // y(data, 'brand')
+    // y(data, 'fullName')
+    // y(data, 'note')
+    // y(data, 'url')
+    // y(data, 'imageUrl')
+    // y(data, 'category')
+    // y(data, 'ingredients')
+    // y(data, 'allergens')
 
     y(data, 'servingSize')
     y(data, 'calories')
@@ -256,6 +280,11 @@ function serialize(data) {
     y(data, 'folate')
     y(data, 'copper')
     y(data, 'calcium')
+
+    y(data, 'consumptionUnit')
+    y(data, 'consumptionGrams')
+
+    // y(data, 'warnings')
 }
 
 
@@ -263,17 +292,26 @@ function y(list, name) {
     let prefix = '  ';
     let stanza = _.find(list, { name: name })
     if (!stanza) {
-        console.log(prefix + name + ': ' + 0);
+        // console.log(prefix + name + ': ' + 0);
         return;
     }
 
     if (Array.isArray(stanza.value)) {
         console.log(prefix + name + ':')
         for (let item of stanza.value) {
-            console.log(prefix + "- " + item)
+            console.log(prefix + '- ' + item)
         }
         return
     }
 
-    console.log(prefix + name + ': ' + stanza.value);
+    if (typeof stanza.value === 'number') {
+        console.log(prefix + name + ': ' + roundToThousandth(stanza.value));
+    } else {
+        console.log(prefix + name + ': ' + stanza.value);
+    }
+}
+
+
+function roundToThousandth(num) {
+    return Math.round(num * 1000) / 1000;
 }
